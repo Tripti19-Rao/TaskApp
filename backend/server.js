a1302267adf1d91f6691715bb48c994860371938
Tripti19-Rao <truptidrao@gmail.com>
2024-01-23 16:42:19 +0530

    Changed the structure of backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 const port = 3052
-const { Schema , model } = mongoose
-const { checkSchema, validationResult } = require('express-validator') 
+const { checkSchema } = require('express-validator') 
 app.use(express.json())
 app.use(cors())
 const configureDB = require('./config/db')
@@ -18,19 +16,6 @@ app.get('/api/tasks',  taskCltr.list)
 app.get('/api/tasks/:id', taskCltr.getone)
 app.delete('/api/tasks/:id',taskCltr.destroy)
 app.put('/api/tasks/:id', checkSchema(taskValidationSchema), taskCltr.update)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen( port , ()=>{
     console.log('Server is running on port '+port)
